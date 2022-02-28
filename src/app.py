09cb7ff90e483f065c931facb2fcfb9df4b26017
d0nal <d0naldhuynhdang@gmail.com>
2022-02-28 01:19:29 -0600

    new lines

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ def main():
         text = tk.Text(window, height=10, width=30)
         text.pack()
         for i in range(1,10):
-                text.insert(tk.END, 'Hello')
+                text.insert(tk.END, 'Hello\n')
         tk.mainloop()
 
 if __name__ == '__main__':
